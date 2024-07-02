ce660845edfd38b85ab5ca2c768cc79d9c6d3e2d
josedev21 <josedev257@gmail.com>
2024-07-02 20:09:34 -0300

    ajustando cotador e bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 AOS.init();
+
+// pega a data do evento
+const dataDoEvento = new Date("Aug 1, 2024 22:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHrs = setInterval(function() {
+    // pega a data de hoje
+    const dataHoje = new Date();
+    const timeStampAtual = dataHoje.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const hrsEmMs = 1000 * 60 * 60;
+    const minEmMs = 1000 * 60 ;
+
+    const DiasAteEvento = Math.floor(distanciaAteEvento / diasEmMs);
+    const  HorasAteEvento = Math.floor(distanciaAteEvento % diasEmMs / hrsEmMs);
+    const MinutosAteEvento = Math.floor(distanciaAteEvento % hrsEmMs / minEmMs);
+    const SegundosAteEvento = Math.floor(distanciaAteEvento % minEmMs / 1000)
+
+    document.getElementById('contador').innerHTML = `${DiasAteEvento} Dias : ${HorasAteEvento} Horas : ${MinutosAteEvento} Minutos : ${SegundosAteEvento} Segundos`
+
+},1000)
